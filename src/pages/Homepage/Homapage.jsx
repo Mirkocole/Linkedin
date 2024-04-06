572f199a7b956c0a471185fdf91f0e29e3b2d440
Mirkocole <mirkocole92@gmail.com>
2024-04-06 20:27:42 +0200

    test

--- src/pages/Homepage/Homapage.jsx
+++ src/pages/Homepage/Homapage.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Homepage() {
+
+  const apiKey = process.env.REACT_APP_AUTHTOKEN;
+  const apiUrl = process.env.REACT_APP_APIURL;
+
+  const [search,setSearch] = useState('');
+
   return (
     <div>Homepage</div>
   )
